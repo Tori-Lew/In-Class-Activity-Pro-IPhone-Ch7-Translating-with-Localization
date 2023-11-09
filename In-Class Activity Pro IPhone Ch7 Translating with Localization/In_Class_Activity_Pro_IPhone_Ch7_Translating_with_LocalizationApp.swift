2bc6418b41070e6bbc699ca28ac5df603c7709f4
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-11-08 16:13:32 -0800

    Initial Commit

---
+++ In-Class Activity Pro IPhone Ch7 Translating with Localization/In_Class_Activity_Pro_IPhone_Ch7_Translating_with_LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch7_Translating_with_LocalizationApp.swift
+//  In-Class Activity Pro IPhone Ch7 Translating with Localization
+//
+//  Created by Student Account on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch7_Translating_with_LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
