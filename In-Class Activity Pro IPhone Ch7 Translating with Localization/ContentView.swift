300e69e99bc4ddd67283f53bfceffe32259d7e0b
Tori Lew <torirlew@gmail.com>
2023-11-15 15:06:22 -0800

    Localization

--- In-Class Activity Pro IPhone Ch7 Translating with Localization/ContentView.swift
+++ In-Class Activity Pro IPhone Ch7 Translating with Localization/ContentView.swift
@@ -8,17 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var changeMessage = true
+    @State var timesPressed = 0
+    @State var name = ""
+    var placeholder: LocalizedStringKey = "placeholder-label"
+    @State var pi = 3.1415
+    let date: Date
+        let dateFormatter: DateFormatter
+        init() {
+            date = Date()
+            dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .medium
+            dateFormatter.timeStyle = .short
+        }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            TextField(placeholder, text: $name)
+                .padding()
+            Text("greeting-label \(name)")
+            Button("button-label", action: {
+                timesPressed += 1
+                print("Times pressed: \(timesPressed)")
+            })
+            Text("\(timesPressed) buttonResult")
+            Text("pi-label \(pi)")
+            Text(date, formatter: dateFormatter)
+        }//.background(changeColor ? Color.red : Color.yellow)
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .environment(\.locale, .init(identifier: "en"))
+            ContentView()
+                .environment(\.locale, .init(identifier: "es"))
+        }
+    }
 }
